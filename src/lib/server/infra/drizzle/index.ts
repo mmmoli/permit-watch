cb0c29e04f9ad31fd7729fb3b4b76ff39a860816
Michele Memoli <michele.memoli@gmail.com>
2023-11-14 21:34:34 +0000

    perf(drizzle): switched to pooled connection
    [skip ci]

--- src/lib/server/infra/drizzle/index.ts
+++ src/lib/server/infra/drizzle/index.ts
@@ -1,16 +1,14 @@
 import { migrate } from 'drizzle-orm/postgres-js/migrator';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { DRIZZLE_DATABASE_URL } from '$env/static/private';
-import { Client } from 'pg';
+import { Pool } from 'pg';
 import { schema } from './schema';
 
-const client = new Client({
+const pool = new Pool({
 	connectionString: DRIZZLE_DATABASE_URL
 });
 
-await client.connect();
-
-export const db = drizzle(client, { schema, logger: true });
+export const db = drizzle(pool, { schema, logger: true });
 export type Db = typeof db;
 
 await migrate(db, { migrationsFolder: './src/lib/server/infra/drizzle/migrations' });
