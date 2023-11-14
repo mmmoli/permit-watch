d190aed9beba82fb37934ccbbf6511aaffc312a4
Michele Memoli <michele.memoli@gmail.com>
2023-11-14 21:35:48 +0000

    perf(drizzle): logging only in dev

--- src/lib/server/infra/drizzle/index.ts
+++ src/lib/server/infra/drizzle/index.ts
@@ -1,6 +1,6 @@
 import { migrate } from 'drizzle-orm/postgres-js/migrator';
 import { drizzle } from 'drizzle-orm/node-postgres';
-import { DRIZZLE_DATABASE_URL } from '$env/static/private';
+import { DRIZZLE_DATABASE_URL, NODE_ENV } from '$env/static/private';
 import { Pool } from 'pg';
 import { schema } from './schema';
 
@@ -8,7 +8,7 @@ const pool = new Pool({
 	connectionString: DRIZZLE_DATABASE_URL
 });
 
-export const db = drizzle(pool, { schema, logger: true });
+export const db = drizzle(pool, { schema, logger: NODE_ENV === 'development' });
 export type Db = typeof db;
 
 await migrate(db, { migrationsFolder: './src/lib/server/infra/drizzle/migrations' });
